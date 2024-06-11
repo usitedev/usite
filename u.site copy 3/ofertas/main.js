9767f08743ac4c72e508f49b53d9fc7cd7449da1
usitedev <grimanjose5@gmail.com>
2024-06-11 11:21:08 -0400

    Add files via upload

---
+++ u.site copy 3/ofertas/main.js
@@ -0,0 +1,53 @@
+const productos = [
+{
+    id:"items-1", 
+    titulo: "items-1",
+    Imagen: "items-1",
+    categoria: 
+    {
+        nombre: "Abrigos",
+        id:"Abrigos"
+    },
+    precio: "1000"
+
+},
+{
+    id:"items-2", 
+    titulo: "items-1",
+    Imagen: "items-1",
+    categoria: 
+    {
+        nombre: "Abrigos",
+        id:"Abrigos"
+    },
+    precio: "1000"
+
+},
+{
+    id:"items-3", 
+    titulo: "items-1",
+    Imagen: "items-1",
+    categoria: 
+    {
+        nombre: "Abrigos",
+        id:"Abrigos"
+    },
+    precio: "1000"
+
+},
+{
+    id:"items-1", 
+    titulo: "items-1",
+    Imagen: "items-1",
+    categoria: 
+    {
+        nombre: "Abrigos",
+        id:"Abrigos"
+    },
+    precio: "1000"
+
+}
+
+
+
+]
